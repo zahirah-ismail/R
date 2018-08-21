4091dd84ec9c754a036a39649c99b492b086c193
Tazmane-PC\zahirah <zahirah.ismail1@gmail.com>
2018-08-21 14:08:30 +0200

    new

--- myR/cars.R
+++ myR/cars.R
@@ -6,6 +6,7 @@ plot(speed~dist,cars)
 fitcars=lm(speed~dist, data=cars)
 fitcars
 summary(cars)
+boxplot(speed~dist,data=cars, main="distance vs speed in cars", xlab="Distance" ,ylab="speed")
 confint(fitcars)
 abline(fitcars,col="green")
 predict(fitcars,data.frame(dist=c(23)), interval="confidence")
--- myR/linear_regression.R
+++ myR/linear_regression.R
@@ -17,3 +17,40 @@ fit3=lm(medv~.,Boston)
 summary(fit3)
 par(mfrow=c(2,2))
 plot(fit3)
+fit4=update(fit3,~.-age-indus)
+### Nonlinear terms and interactions
+fit5=lm(medv~lstat*age,Boston)
+summary(fit5)
+fit6=lm(medv~lstat +I(lstat^2),Boston); summary(fit6)
+attach(Boston)
+par(mfrow=c(1,1))
+plot(medv~lstat)
+points(lstat,fitted(fit6),col="red",pch=20)
+fit7=lm(medv~poly(lstat,4))
+points(lstat,fitted(fit7),col="blue",pch=20)
+plot(1:20,1:20,pch=1:20,cex=2)
+### Qualitative predictors
+fix(Carseats)
+names(Carseats)
+summary(Carseats)
+fit1=lm(Sales~.+Income:Advertising+Age:Price,Carseats)
+summary(fit1)
+contrasts(Carseats$ShelveLoc)## dummy variables
+##Writing R functions
+regplot=function(x,y){
+  fit=lm(x~y)
+  plot(x,y)
+  abline(fit,col="red")
+}
+
+attach(Carseats)
+regplot(Price,Sales)
+
+regplot=function(x,y,...){
+  fit=lm(x~y)
+  plot(x,y,...)
+  abline(fit,col="red")
+}
+
+regplot(Price,Sales,xlab="Price" , ylab="Sales", col="blue",pch=20)
+
